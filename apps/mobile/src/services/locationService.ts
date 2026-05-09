9b2f61e1217824e85ad0fb6c1b74102da9454f38
ahmetdervissarar <ahmetdervissarar@gmail.com>
2026-05-09 15:48:28 +0300

    Add location service skeleton for pricing

---
+++ apps/mobile/src/services/locationService.ts
@@ -0,0 +1,9 @@
+export async function getUserLocationForPricing(): Promise<{
+  latitude: number;
+  longitude: number;
+} | null> {
+  // TODO: İleride expo-location ile izin istenecek.
+  // TODO: İleride latitude ve longitude alınacak.
+  // TODO: İleride market fiyatı sorgusunda kullanılacak.
+  return null;
+}
